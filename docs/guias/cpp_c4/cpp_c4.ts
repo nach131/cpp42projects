67e2ec5b9a8e9d122ace501c116a3374dc97bf89
Nacho Mota 🚀 <nacho.mota@gmail.com>
2023-12-03 20:39:31 +0100

    number svg

--- docs/guias/cpp_c4/cpp_c4.ts
+++ docs/guias/cpp_c4/cpp_c4.ts
@@ -1,19 +1,19 @@
 export const module00 = [
   {
-    icon: "/icons/number/0.svg",
-    title: "Module 00",
+    icon: "/cpp42projects/icons/number/0.svg",
+    title: "Module",
     description: "Namespaces, class, funciones miembro, stdio streams, lista de inicialización, static, const, y montones de cosas basicas",
     link: "/cpp42projects/guias/cpp_c4/module00/"
   },
   {
-    icon: "/icons/info/2.svg",
-    title: "Module 01",
+    icon: "/cpp42projects/icons/number/1.svg",
+    title: "Module",
     description: "Reserva de memoria, punteros a miembros, referencias, switch",
     link: "/cpp42projects/guias/cpp_c4/module01/",
   },
   {
-    icon: "/icons/info/3.svg",
-    title: "Module 02",
+    icon: "/cpp42projects/icons/number/2.svg",
+    title: "Module",
     description: "Polimorfismo ad-hoc, sobrecargas y clases canónico-ortodoxas en C++",
     link: "/cpp42projects/guias/cpp_c4/module02/",
   }
